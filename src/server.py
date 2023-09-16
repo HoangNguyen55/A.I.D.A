765a5a26d5bb0de2c10436fb526600058aaf3127
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-09-15 22:01:47 -0400

    add basic autherization

--- src/server.py
+++ src/server.py
@@ -1,9 +1,23 @@
+from http import HTTPStatus
 import asyncio
 import websockets
 
 ADDRESS = "localhost"
 PORT = "5172"
 
+# TODO implement TLS
+
+async def process_request(path, header):
+    try:
+        # TODO implment database fetching for password hashes, salt, peper, etc...
+        auth = header['Authorization']
+        if auth:
+            return None
+    except KeyError:
+        message = b'Authorization failed'
+    
+    return (HTTPStatus.UNAUTHORIZED, 
+            {'WWW-Authenticate': 'Basic realm="Access To A.I.D.A"'})
 
 async def handler(websocket):
     async for message in websocket:
@@ -14,7 +28,7 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, ADDRESS, PORT):
+    async with websockets.serve(handler, ADDRESS, PORT, process_request=process_request):
         await asyncio.Future()
 
 if __name__ == "__main__":
