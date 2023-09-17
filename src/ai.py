5b77490104d8704217ba1eebe2835e32903fd44f
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-09-17 16:08:26 -0400

    Fix missing self

--- src/ai.py
+++ src/ai.py
@@ -1,23 +1,28 @@
+import logging
 from transformers import AutoModelForCausalLM
 import transformers
 
-class AI():
-    def __init__(self):
-        model_path = "/model"
-        
+
+class AI:
+    def __init__(self, model_path):
+        self.model_path = model_path
+        self.started = False
+
+    def initialize(self):
         config = transformers.AutoConfig.from_pretrained(
-            model_path,
-            rope_scaling={} # llama 2 specific? optimization setting
+            self.model_path, rope_scaling={}  # llama 2 specific? optimization setting
         )
-        
         # TODO add some more optins, 4 bits quantizations, etc...
-        model = AutoModelForCasualLM.from_config(config)
-        tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+        self.model = AutoModelForCausalLM.from_config(config)
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_path)
 
-
-    def feed_input(prompt: str):
+    def feed_input(self, prompt: str, system_prompt: str = ""):
+        if not self.started:
+            logging.critical("AI have not been started yet")
+            return
         # https://huggingface.co/docs/transformers/v4.33.0/en/llm_tutorial#common-pitfalls
-        input = tokenizer(SYSTEM_PROMPT + PROMPT, return_tensors="pt").to("cuda")
-        generated_ids = model.generate(**input)
-        output = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        return ouput
+        # TODO add token streaming
+        input = self.tokenizer(system_prompt + prompt, return_tensors="pt").to("cuda")
+        generated_ids = self.model.generate(**input)
+        output = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        return output
