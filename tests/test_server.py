3018a9ed28ecc6c1232a0a3c67551a2ddd744ed0
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-09-18 16:21:35 -0400

    Add more tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -10,26 +10,72 @@ from server import process_request
 
 pytest_plugins = ('pytest_asyncio',)
 
+
 @pytest.mark.asyncio
-async def test_login_endpoint_pass():
-    # Prepare a mock Headers object with Authorization header
+async def test_process_request_no_auth():
+    headers = Headers()
+    # Call the process_request function to simulate a request to /login
+    response = await process_request("", headers)
+    assert response != None, "Should be rejects"
+
+    response_status, response_header, response_body = response
+    assert response_status == HTTPStatus.UNAUTHORIZED, "Should be unauthorized"
+
+    assert response_header == {
+            "WWW-Authenticate": 'Basic realm="Access To A.I.D.A"'
+            }, "Should be {'WWW-Authenticate': 'Basic realm=\"Access To A.I.D.A\"}"
+
+    assert response_body == b"Authorization field is missing",\
+            "Should be 'Authorization field is missing'"
+
+
+@pytest.mark.asyncio
+async def test_process_request_wrong_path():
     headers = Headers()
     # base64("username:password")
     headers["Authorization"] = "Basic dXNlcm5hbWU6cGFzc3dvcmQ="  
 
-    # Call the process_request function to simulate a request to /login
+    response = await process_request("/doest_exist", headers)
+    assert response != None, "Should be rejects"
+
+    response_status, _, response_body = response
+    assert response_status == HTTPStatus.NOT_FOUND, "Should be unauthorized"
+    assert response_body == b"Path does not exist", "Should be 'Path does not exist'"
+
+
+
+@pytest.mark.asyncio
+async def test_login_wrong_account():
+    headers = Headers()
+    headers["Authorization"] = "Basic dXNlcm5hbWU6d3JvbmdfcGFzc3dvcmQ="  
+    response = await process_request("/login", headers)
+    assert response != None, "Should fail the login"
+
+    response_status, _, response_body = response
+    assert response_status == HTTPStatus.UNAUTHORIZED, "Should be unauthorized"
+    assert response_body == b"Account does not exist", "Should be 'Account does not exist'"
+
+
+@pytest.mark.asyncio
+async def test_login_pass():
+    headers = Headers()
+    headers["Authorization"] = "Basic dXNlcm5hbWU6cGFzc3dvcmQ="  
+
     response = await process_request("/login", headers)
     assert response == None, "Should pass the login"
 
 
 @pytest.mark.asyncio
-async def test_login_endpoint_fail():
-    # Prepare a mock Headers object with Authorization header
+async def test_signup():
     headers = Headers()
-    # Call the process_request function to simulate a request to /login
-    response = await process_request("/login", headers)
+    headers["Authorization"] = "Basic dXNlcm5hbWU6cGFzc3dvcmQ="  
+
+    response = await process_request("/signup", headers)
     assert response != None, "Should fail the login"
 
-    response_status, _, _ = response
-    # Check the expected behavior
-    assert response_status == HTTPStatus.UNAUTHORIZED, "Should be unauthorized"
+    response_status, _, response_body = response
+    assert response_status == HTTPStatus.ACCEPTED, "Should be accepted"
+    assert response_body == \
+            b"Your sign up request have been recieved\nPlease wait for admin approval",\
+            "Should be 'Your sign up request have been recieved\nPlease wait for admin approval'"
+
