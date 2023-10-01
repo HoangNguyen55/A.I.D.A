0956c8038aaf5e85dd4eac396d7742a67f0cb92f
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-09-30 21:00:57 -0400

    Follow python naming for private variable

--- src/ai.py
+++ src/ai.py
@@ -1,28 +1,35 @@
 import logging
-from transformers import AutoModelForCausalLM
-import transformers
+import torch
+from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
 
 
 class AI:
     def __init__(self, model_path):
-        self.model_path = model_path
-        self.started = False
+        self._model_path = model_path
+        self._started = False
+        self._model
+        self._tokenizer
 
-    def initialize(self):
-        config = transformers.AutoConfig.from_pretrained(
-            self.model_path, rope_scaling={}  # llama 2 specific? optimization setting
+    def start(self):
+        config = AutoConfig.from_pretrained(
+            self._model_path, rope_scaling={}  # llama 2 specific? optimization setting
         )
         # TODO add some more optins, 4 bits quantizations, etc...
-        self.model = AutoModelForCausalLM.from_config(config)
-        self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_path)
+        self._model = AutoModelForCausalLM.from_pretrained(
+            self._model_path, config=config, local_files_only=True
+        )
+        self._tokenizer = AutoTokenizer.from_pretrained(self._model_path)
+        self._started = True
 
+
+    # TODO add async
     def feed_input(self, prompt: str, system_prompt: str = "") -> str:
-        if not self.started:
-            logging.critical("AI have not been started yet")
+        if not self._started:
+            logging.warn("AI have not been started yet")
             return ""
         # https://huggingface.co/docs/transformers/v4.33.0/en/llm_tutorial#common-pitfalls
         # TODO add token streaming
-        input = self.tokenizer(system_prompt + prompt, return_tensors="pt").to("cuda")
-        generated_ids = self.model.generate(**input)
-        output = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        return output
+        input = self._tokenizer(system_prompt + prompt, return_tensors="pt").to("cuda")
+        generated_ids = self._model.generate(**input)
+        output = self._tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        return output[0]
