d002801b833a138740233ee65dc862a64b7e524f
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-11-10 17:48:29 -0500

    pass cli args to main

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,9 @@
 import sys
+from configargparse import Namespace
 from websockets.frames import CloseCode
 from websockets.typing import Data
 from .ai import AI
-from .database.access import DBAccess
+from .database import DBAccess
 from .error import MessageTooBigError
 from .datatype import ConnectionType, Credentials, RecieveData
 import logging
@@ -13,8 +14,9 @@ from websockets.server import WebSocketServerProtocol, serve
 from argon2 import PasswordHasher
 from argon2.exceptions import VerificationError, VerifyMismatchError
 
+CLI_OPTIONS: Namespace
+DB: DBAccess
 PASSHASH = PasswordHasher()
-DB = DBAccess("db_config.ini")
 
 # 1 mebibyte
 MAX_DATA_SIZE = 1048576
@@ -38,6 +40,7 @@ async def handle_connection(websocket: WebSocketServerProtocol):
         logging.debug(f"New connection purpose: {creds.connection_type}")
 
         if creds.connection_type == ConnectionType.LOGIN:
+            # TODO get userdata here
             # user_data = DB.get_user(creds.email)
             # PASSHASH.verify(creds.password, user_data[2])
             logging.debug(f"Connection logged in with name: {creds.email}")
@@ -47,6 +50,7 @@ async def handle_connection(websocket: WebSocketServerProtocol):
 
         elif creds.connection_type == ConnectionType.SIGNUP:
             logging.debug(f"New connection is signing up with the email: {creds.email}")
+            # TODO add sign up here
             await websocket.close(
                 CloseCode.TRY_AGAIN_LATER,
                 "Sign up complete, please wait for admin approval",
@@ -65,6 +69,9 @@ async def handle_connection(websocket: WebSocketServerProtocol):
     except MessageTooBigError as err:
         logging.info(f"Connection fail to authenticate: {str(err)}")
         await websocket.close(CloseCode.MESSAGE_TOO_BIG, str(err))
+    except Exception as err:
+        logging.error(f"Unknown error happened: {str(err)}")
+        await websocket.close(CloseCode.INTERNAL_ERROR)
 
 
 async def handle_data(websocket: WebSocketServerProtocol, token: str):
@@ -76,6 +83,10 @@ async def handle_data(websocket: WebSocketServerProtocol, token: str):
         await websocket.send(token)
 
 
-async def start_server(address: str, port: int):
-    async with serve(handle_connection, address, port):
+async def start_server(cli_options: Namespace):
+    global CLI_OPTIONS, DB
+    CLI_OPTIONS = cli_options
+    DB = DBAccess(cli_options.db_path)
+
+    async with serve(handle_connection, cli_options.address, cli_options.port):
         await asyncio.Future()
