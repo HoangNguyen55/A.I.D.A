a8fea33c4ac225248df74bab27021b90b80fc5d8
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-11-12 12:39:21 -0500

    update docstring

--- src/server/database.py
+++ src/server/database.py
@@ -20,6 +20,18 @@ class DBAccess:
             exit(1)
 
     def get_user_password(self, email: str) -> tuple[str, str]:
+        """
+        Retrieve the UUID and password associated with the given email address from the 'users' table.
+
+        Parameters:
+        - email (str): The email address of the user.
+
+        Returns:
+        tuple[str, str]: A tuple containing the UUID and password if the user exists, otherwise raises UserDoesNotExist.
+
+        Raises:
+        UserDoesNotExist: If the user with the provided email does not exist.
+        """
         try:
             self._cursor.execute(
                 f"SELECT uuid, password FROM users WHERE email = ?", (email)
@@ -33,6 +45,18 @@ class DBAccess:
         raise UserDoesNotExist
 
     def get_user_data(self, uuid: str) -> UserData:
+        """
+        Retrieve the username and system prompt associated with the given UUID from the 'users' table.
+
+        Parameters:
+        - uuid (str): The UUID of the user.
+
+        Returns:
+        UserData: An instance of UserData containing the username and system prompt.
+
+        Raises:
+        UnknownError: If an unexpected error occurs during the retrieval process.
+        """
         try:
             self._cursor.execute(
                 "SELECT username, system_prompt FROM users WHERE uuid = ?",
@@ -45,17 +69,19 @@ class DBAccess:
             logging.error(f"Error getting system prompt: {e}")
             raise UnknownError("Something unexpected happened")
 
-    def add_user_pending_approve(self, username, password, email) -> None:
+    def add_user_pending_approve(
+        self, username: str, password: str, email: str
+    ) -> None:
         """
-        Add a new user to the pending users table in the database for approval.
+        Add a new user to the 'pending_users' table for approval.
 
-        Args:
-            username (str): The username of the new user.
-            password (str): The password of the new user.
-            email (str): The email address of the new user.
+        Parameters:
+        - username: The username of the new user.
+        - password: The password of the new user.
+        - email: The email address of the new user.
 
-        Returns:
-            None
+        Raises:
+        UnknownError: If an unexpected error occurs during the insertion process.
         """
         try:
             self._cursor.execute(
@@ -67,7 +93,18 @@ class DBAccess:
             logging.error(f"Error adding a new user: {e}")
             raise UnknownError("Something unexpected happened")
 
-    def add_user(self, username, password, email) -> None:
+    def add_user(self, username: str, password: str, email: str) -> None:
+        """
+        Add a new user to the 'users' table.
+
+        Parameters:
+        - username: The username of the new user.
+        - password: The password of the new user.
+        - email: The email address of the new user.
+
+        Raises:
+        UnknownError: If an unexpected error occurs during the insertion process.
+        """
         try:
             self._cursor.execute(
                 "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
