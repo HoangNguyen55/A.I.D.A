dac3c7301cee433397a741b753b36da20876beb1
Mikheil-U <uglavamisho@gmail.com>
2023-11-12 20:46:50 -0500

    Add ApprovedUser model, remove unnecessary files

--- src/aida-admin/aida_admin_app/admin.py
+++ src/aida-admin/aida_admin_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import PendingUser
+from .models import PendingUser, ApprovedUser
 from .forms import PendingUserForm
 from django.contrib.auth import get_user_model
-
+import uuid
 
 @admin.register(PendingUser)
 class PendingUserAdmin(admin.ModelAdmin):
@@ -16,15 +16,18 @@ class PendingUserAdmin(admin.ModelAdmin):
 
         # Iterate through selected PendingUser objects
         for pending_user in queryset:
-            # Create a new User object with the same data
-            user = User.objects.create_user(
+            # Create a new ApprovedUser object with the same data
+            approved_user = ApprovedUser.objects.create(
+                id = uuid.uuid4(),
                 username=pending_user.username,
                 password=pending_user.password,
                 email=pending_user.email,
-
             )
 
             # Delete the PendingUser
             pending_user.delete()
 
         self.message_user(request, 'Selected users have been approved and moved to User model.')
+
+
+admin.site.register(ApprovedUser)
