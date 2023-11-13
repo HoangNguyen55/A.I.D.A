7a24586833456e85d10a9cb641b7c6316da2b1d9
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-11-13 15:09:23 -0500

    more gpus

--- psu-installs/install-psu-step1.sh
+++ psu-installs/install-psu-step1.sh
@@ -11,4 +11,4 @@ git clone --depth 1 https://github.com/HoangNguyen55/A.I.D.A
 [ -d "$slurm_path/A.I.D.A" ] && rm -rf "$slurm_path/A.I.D.A"
 cp -r ./A.I.D.A  $slurm_path
 
-srun --nodes=1 --ntasks-per-node=1 --gres=gpu:1 --time=1:00:00 --pty $step_2
+srun --gpus=3 --time=1:00:00 --pty $step_2
