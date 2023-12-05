39c9325ce26145023a70f64557ebf92736fae393
xiangliu1123 <117769320+xiangliu1123@users.noreply.github.com>
2023-12-05 17:41:58 -0500

    Update test_aiModel.py

--- tests/test_aiModel.py
+++ tests/test_aiModel.py
@@ -1,41 +1,78 @@
-from huggingface_hub import login
-token = "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"
-login(token)
+from random import randint
+from transformers import AutoModelForCausalLM
+from transformers import AutoTokenizer
+import time
+from transformers import LlamaTokenizer
+from transformers import LlamaForCausalLM
+from transformers import AutoConfig,TextStreamer
+from langdetect import detect, detect_langs
 
-from datasets import load_dataset
-import torch
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer, TrainingArguments
-from peft import LoraConfig
-from trl import SFTTrainer
-import transformers
-from transformers import LlamaModel, LlamaConfig
+while True:
+  
+    prompt = "Enter Prompt:  "
+    user_prompt = input(prompt)
+    texts = [user_prompt]
+    # prompt = ""
+    # <s>[INST] <<SYS>>
+    # {{ system_prompt }}
+    # <</SYS>>
 
-configuration = LlamaConfig()
+    # {{ user_message }} [/INST]
+    # https://www.philschmid.de/llama-2#how-to-prompt-llama-2-chat 
 
 
-model = LlamaModel(configuration)
-configuration = model.config
 
-tokenizer = AutoTokenizer.from_pretrained("/home/xbl5229/llama/llama-2-7b-chat")
-pipeline = transformers.pipeline(
-    "text-generation",
-    model="/home/xbl5229/llama/llama-2-7b-chat",
-    torch_dtype=torch.float16,
-    device_map="auto", )
+    def langdetect(text):
+        languages = []
+        for text in texts:
+            languages.append((detect_langs(text)))
+        return languages
+    ens = False
+    try:
+        x = langdetect(user_prompt)
+        if 'en' in str(x[0][0]):
+            ens= True
+    except:
+        ens= True
 
-input = 'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n'
-def IPnOP (input: str)-> None:
-    sequences = pipeline(
-        input,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=200,
-    )
+    repoid= '/slrmstore/xbl5229/Finetuned_Model'
 
-    for seq in sequences:
-        print(f"Result: {seq['generated_text']}")
 
-print(IPnOP(input))
+    model = AutoModelForCausalLM.from_pretrained(
+        repoid, device_map="auto", load_in_4bit=True)
+
+    token = AutoTokenizer.from_pretrained(repoid,padding_side="left")
+
+
+
+    if ens==False :
+        sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct"
+    else:
+        if randint(1,2)==2:
+            sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct"
+        else:
+            sys_prompt = "You are an assistant, answer questions as condescending, passive-aggressive and mean as possible, but make sure to give correct informations."
+
+        
+
+
+    complete_prompt = f"""
+            <s>[INST] <<SYS>>
+            {sys_prompt}
+            <</SYS>>
+            
+            {user_prompt} [/INST]
+
+            """
+
+
+    model_inputs = token(
+        [complete_prompt], return_tensors="pt",truncation=True, max_length = 1028).to("cuda")
+    streamer = TextStreamer(token,skip_prompt=True)
+    generated_ids = model.generate(**model_inputs, do_sample=True, max_new_tokens=1000, streamer=streamer)
+    # output = token.batch_decode(generated_ids,skip_special_tokens=True)
+
+
+
+
 
