4d31b8bc33524e10fa05c64bf4702b45b08febc4
xiangliu1123 <117769320+xiangliu1123@users.noreply.github.com>
2023-12-06 01:51:28 -0500

    Update test_aiModel.py

--- tests/test_aiModel.py
+++ tests/test_aiModel.py
@@ -1,15 +1,17 @@
 from random import randint
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
-import time
 from transformers import LlamaTokenizer
 from transformers import LlamaForCausalLM
-from transformers import AutoConfig,TextStreamer
+from transformers import AutoConfig,TextStreamer, BitsAndBytesConfig
 from langdetect import detect, detect_langs
+from accelerate.utils import release_memory
+from transformers import LogitsProcessorList, MinLengthLogitsProcessor, RepetitionPenaltyLogitsProcessor
+
+
 
 while True:
-  
-    prompt = "Enter Prompt:  "
+    prompt = "Enter Prompt:"
     user_prompt = input(prompt)
     texts = [user_prompt]
     # prompt = ""
@@ -34,43 +36,46 @@ while True:
             ens= True
     except:
         ens= True
+    
 
-    repoid= '/slrmstore/xbl5229/Finetuned_Model'
-
+    repoid= '/slrmstore/z-common/TunedModel-big'
+    
 
     model = AutoModelForCausalLM.from_pretrained(
-        repoid, device_map="auto", load_in_4bit=True)
-
-    token = AutoTokenizer.from_pretrained(repoid,padding_side="left")
-
+        repoid, device_map="auto", load_in_4bit=True, local_files_only=True)
+    
+    
+    token = LlamaTokenizer.from_pretrained(repoid, padding = 'right')
 
 
     if ens==False :
-        sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct"
-    else:
+        sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct and do not give emojis."
         if randint(1,2)==2:
-            sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct"
-        else:
-            sys_prompt = "You are an assistant, answer questions as condescending, passive-aggressive and mean as possible, but make sure to give correct informations."
+            sys_prompt = "You are a nice assistant, answer questions as polite as possible and be correct and do not give emojis."
+    else:
+        sys_prompt = "You are an assistant, answer questions as condescending, passive-aggressive and mean as possible, but make sure to give correct informations and do not give emojis."
 
         
 
 
     complete_prompt = f"""
-            <s>[INST] <<SYS>>
-            {sys_prompt}
-            <</SYS>>
-            
-            {user_prompt} [/INST]
+                    <s>[INST] <<SYS>>
+                    {sys_prompt}
+                    <</SYS>>
+                    
+                    {user_prompt} [/INST]
 
-            """
-
+                    """
 
     model_inputs = token(
-        [complete_prompt], return_tensors="pt",truncation=True, max_length = 1028).to("cuda")
+        [complete_prompt], return_tensors="pt",padding= True, truncation=True, max_length = 2048).to("cuda")
     streamer = TextStreamer(token,skip_prompt=True)
-    generated_ids = model.generate(**model_inputs, do_sample=True, max_new_tokens=1000, streamer=streamer)
-    # output = token.batch_decode(generated_ids,skip_special_tokens=True)
+    generated_ids = model.generate(**model_inputs, do_sample=True, max_new_tokens=500, streamer=streamer, temperature=0.7,  # Adjusted temperature
+    top_k=60,         # Adjusted top-k sampling
+    top_p=0.95      # Adjusted top-p (nucleus) sampling
+    )
+
+    release_memory(model)
 
 
 
